210925ebfc6051181d8df053e5bb128df575013a
Максим <simdok25.s@gmail.com>
2025-11-13 14:25:54 +0200

    Init

---
+++ src/main/java/com/msk/salaryflow/SalaryflowApplication.java
@@ -0,0 +1,13 @@
+package com.msk.salaryflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msk/salaryflow/SalaryflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msk.salaryflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
