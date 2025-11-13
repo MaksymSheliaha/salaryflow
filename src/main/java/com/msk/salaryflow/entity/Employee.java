646fb620178bec8240b054e5730197a1123d424a
Максим <simdok25.s@gmail.com>
2025-11-13 14:49:54 +0200

    Add docker compose, base entity, change application properties

---
+++ src/main/java/com/msk/salaryflow/entity/Employee.java
@@ -0,0 +1,15 @@
+package com.msk.salaryflow.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class Employee {
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private double salary;
+    private Instant hireDate;
+
+}
