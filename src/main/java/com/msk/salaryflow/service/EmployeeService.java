944ed41750171cffb52a0650e1bd11cafe383ba3
SeliverstovaKaryna <seliverstovaky@gmail.com>
2025-11-21 15:38:00 +0200

    Fixed EmployeeController

--- src/main/java/com/msk/salaryflow/service/EmployeeService.java
+++ src/main/java/com/msk/salaryflow/service/EmployeeService.java
@@ -7,6 +7,7 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
